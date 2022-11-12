acdfeb9f6b5f74cf842f78a7e1c361d0280c4cab
pica fichu <picafichu@gmail.com>
2022-11-12 09:38:45 -0800

    refactor: - access dataframe with columns instead of index - cast data type before insert into db

--- etl.py
+++ etl.py
@@ -6,46 +6,51 @@ from sql_queries import *
 import numpy as np
 
 
+
 def process_song_file(cur, filepath):
     """
-    Description: This function is responsible for reading data from
+    Description: This function is responsible for reading data from 
         log file, trasforms the data and insets it into table
         songs, artists.
-
+        
     Input Arguments:
          cur: database cursor obj.
          filepath: file path
-
-    Returns:
+        
+    Returns: 
         None
     """
     # open song file
     df_songs = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = [df_songs.values[0, 6], df_songs.values[0, 7],
-                 df_songs.values[0, 1], df_songs.values[0, 9],
-                 df_songs.values[0, 8]]
-    cur.execute(song_table_insert, song_data)
+#     song_data = [df_songs.song_id.astype(), df_songs.title,
+#                  df_songs.artist_id, df_songs.year,
+#                  df_songs.duration]
+    
+    song_data_ = [str(df_songs.song_id), str(df_songs.title),
+                 str(df_songs.artist_id), int(df_songs.year),
+                 float(df_songs.duration)]
+    cur.execute(song_table_insert, song_data_)
 
     # insert artist record
     artist_data = [df_songs.values[0, 1], df_songs.values[0, 5],
-                   df_songs.values[0, 4], df_songs.values[0, 3],
-                   df_songs.values[0, 2]]
+               df_songs.values[0, 4], df_songs.values[0, 3],
+               df_songs.values[0, 2]]
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     """
-    Description: This function is responsible for reading data from
+    Description: This function is responsible for reading data from 
         log file, trasforms the data and insets it into table
         time, users, songplays
 
     Arguments:
         cur: database cursor obj.
         filepath: file path
-
-    Returns:
+        
+    Returns: 
         None
     """
     # open log file
@@ -62,8 +67,7 @@ def process_log_file(cur, filepath):
                  t_data.isocalendar().week,
                  t_data.month, t_data.year,
                  t_data.weekday]
-    column_labels = ['timestamp', 'hour', 'day',
-                     'week', 'month', 'year', 'weekday']
+    column_labels = ['timestamp', 'hour', 'day', 'week', 'month', 'year', 'weekday']
     time_df = pd.DataFrame(np.array(time_data).T, columns=column_labels)
 
     for i, row in time_df.iterrows():
@@ -71,10 +75,10 @@ def process_log_file(cur, filepath):
 
     # load user table
     user_df = pd.DataFrame(np.array([df.userId, df.firstName,
-                                     df.lastName, df.gender, df.level]).T,
-                           columns=['user ID', 'first name',
-                                    'last name', 'gender', 'level']
-                           )
+                        df.lastName, df.gender, df.level]).T,
+                        columns = ['user ID', 'first name',
+                                   'last name', 'gender', 'level']
+                      )
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -96,14 +100,14 @@ def process_log_file(cur, filepath):
         songplay_data = (row.ts, row.userId,
                          row.level, songid, artistid,
                          row.sessionId, row.location,
-                         row.userAgent)
+                         row.userAgent )
         cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
     """
-    Description: This function is responsile for loading data from
-        log or song files,coverting them with specified func
+    Description: This function is responsile for loading data from 
+        log or song files,coverting them with specified func 
         and inserting it into database.
 
     Arguments:
@@ -111,15 +115,15 @@ def process_data(cur, conn, filepath, func):
         conn: connection to the db
         filepath: file path
         func: function to covert the data and inserts it into the db
-
-    Returns:
+        
+    Returns: 
         None
     """
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root, '*.json'))
-        for f in files:
+        files = glob.glob(os.path.join(root,'*.json'))
+        for f in files :
             all_files.append(os.path.abspath(f))
 
     # get total number of files found
@@ -141,10 +145,9 @@ def main():
     - Process files and load data into tables.
 
     - Closes the connection.
-
+    
     """
-    conn = psycopg2.connect(
-        "host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
